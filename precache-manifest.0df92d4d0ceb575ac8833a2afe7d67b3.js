54fcd41983718ed3f37ba518e13c7c0543b1d09a
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-03-29 12:10:30 -0500

    Updates

---
+++ precache-manifest.0df92d4d0ceb575ac8833a2afe7d67b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311690cdcd170c6e85d541a985c54ed6",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "ca7ab12befc482cf7b0e",
+    "url": "/reactPortfolio/static/css/main.2c2450f1.chunk.css"
+  },
+  {
+    "revision": "98e1c0032e7cea17b052",
+    "url": "/reactPortfolio/static/js/2.c60bcf84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.c60bcf84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7ab12befc482cf7b0e",
+    "url": "/reactPortfolio/static/js/main.c2832873.chunk.js"
+  },
+  {
+    "revision": "c72dbbafb09bb347ade2",
+    "url": "/reactPortfolio/static/js/runtime-main.d8718c79.js"
+  }
+]);

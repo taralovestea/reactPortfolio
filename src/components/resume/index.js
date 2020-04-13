fe86afde3ca8b81247a6065e916c09dca1ce901e
tara <59029999+taralovestea@users.noreply.github.com>
2020-04-13 13:47:29 -0500

    updated linkedIN

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -11,21 +11,21 @@ return <div class="section-dark resume">
     >
         <div class="column is-12 about-me">
             <h1 class="title has-text-centered section-title">
-                View My Resume
+                View My LinkedIn
 </h1>
         </div>
         <div class="column is-10 has-text-centered is-offset-1">
             <h2 class="subtitle">
                 My experience is a culmination of my love of adventure and my sense of purpose.
 </h2>
-            <a href="path_to_file" download="file:///Users/taraaelisabeth/Desktop/My_New_Resume.pdf"> </a>
-            <button class="button">
-                Download Resume&emsp;<i class="fad fa-download fa-lg"></i>
-            </button>
+    <form action="https://www.linkedin.com/in/taraelisabethhughes/">
+                <button class="button">
+                Go to LinkedIn &emsp;<i class="fad fa-download fa-lg"></i>
+                </button>
 
-        </div>
+        </form>
     </div>
 </div>
 </div>
-
+</div>
 }
